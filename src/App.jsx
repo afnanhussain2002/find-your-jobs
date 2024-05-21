d1654cc594214dabbe3abed995b9faa10f701978
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-05-21 10:57:57 +0600

    Added navbar and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 
+import { Outlet } from 'react-router-dom'
 import './App.css'
+import Header from './shaerd/header/Header'
+import Footer from './shaerd/footer/Footer'
 
 function App() {
 
 
   return (
     <>
-      
+      <Header/>
+      <div className='min-h-screen'>
+        <Outlet/>
+      </div>
+      <Footer/>
     </>
   )
 }
---
+++ src/pages/appliedJobs/AppliedJobs.jsx
@@ -0,0 +1,11 @@
+
+
+const AppliedJobs = () => {
+    return (
+        <div>
+            This is applied Jobs
+        </div>
+    );
+};
+
+export default AppliedJobs;
